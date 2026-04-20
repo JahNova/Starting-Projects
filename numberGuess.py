2ff0b38d0dea2eaa033d788eb3966f8e7d97a780
JahNova <dannel.evans2@gmail.com>
2026-04-20 10:56:25 -0400

    Create numberGuess.py

---
+++ numberGuess.py
@@ -0,0 +1,11 @@
+num=9
+guess_count=0
+guess_limit=3
+while guess_count < guess_limit:
+   guess=int(input('Guess: '))
+   guess_count += 1
+   if int(guess) == num:
+       print('You Win!')
+       break
+   elif guess_count == 3:
+       print('You Lose!')
